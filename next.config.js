f15744408389218bd2e601c36edebbdfa91636d6
LxhNolan <hprnolan1649@foxmail.com>
2025-03-17 02:33:42 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -232,13 +232,3 @@ const nextConfig = {
 module.exports = process.env.ANALYZE
   ? withBundleAnalyzer(nextConfig)
   : nextConfig
-
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      exclude: /public\/videos/,  // 忽略视频目录下的.ts文件
-    });
-    return config;
-  },
-};
