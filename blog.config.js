23ff9281559490f533bf982f8c7e3ae6c8d5e048
LxhNolan <hprnolan1649@foxmail.com>
2025-05-27 22:35:08 +0800

    Update blog.config.js
    修改网站主题样式

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
     '02ab3b8678004aa69e9e415905ef32a5,en:7c1d570661754c8fbc568e00a01fd70e',
-  THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
+  THEME: process.env.NEXT_PUBLIC_THEME || 'next', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2024, // e.g if leave this empty, current year will be used.
 
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'LxhNolan', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个热衷于安稳又幸福还对铁道有亿点痴迷的博主🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://happinessrailway.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客,铁道,生活,记录,幸福,痴迷,BVE,Traincrew,Railroute,铁路调度模拟器,Bve Trainsim,BveTs,火车模拟器,济北铁道,BVE线路开发,BVE线路制作', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客,铁道,生活,记录,幸福,痴迷,BVE,Traincrew,Railroute,铁路调度模拟器,Bve Trainsim,BveTs,火车模拟器,济北铁道,BVE线路开发,BVE线路制作,BVE5', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
